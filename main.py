9a3f5ed70c1f7483cd9509714733648cec557e29
StasyDim <159949504+StasyDim@users.noreply.github.com>
2026-06-23 21:16:57 +0300

    final project

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
-def instrument_model(data):
-    return {
-        "description": data.get("description"),
-        "invNumber": data.get("invNumber"),
-        "serialNumber": data.get("serialNumber"),
-        "location": data.get("location"),
-        "calibrations": data.get("calibrations", [])
-    }
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    print("ROUTE WORKS")
+    return "SKalibro is running!"
+
+if __name__ == "__main__":
+    print("STARTING SERVER...")
+    app.run(debug=True)
